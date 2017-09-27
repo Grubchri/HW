802fc75b0aaed9643a15729079f8822ef9da4f21
Grubchri <chgr2@gmx.at>
2017-09-27 10:22:17 +0200

    test

---
+++ HUE_4_01_1/src/HUE_4_01_1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.net.*;
+
+public class HUE_4_01_1{
+
+    
+    public static void main(String[] args) throws IOException {
+        
+        MultiThread mt=new MultiThread();
+        mt.runServer();
+
+        
+    }
+    
+}
